eef10b6247dfd9c60ab75a91481845dfc547ca6c
kabine kaba <kabine92@gmail.com>
2020-05-04 03:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments)  {
+var x = [ ] ;
+for (var i = 0; i < musicians.length; i++) {
+  x.push(musicians[i] + 'play' + instruments[i])
+}
+return x;
+}
+
+function johnLennonFacts ( ) {
+  
+}
